319a38f131137354ebe7e2173d7fbc3828826387
Oleg Vukadinov <153370690+OlegVukadinov@users.noreply.github.com>
2023-12-30 19:41:51 +0200

    Update 10_perfect_number.py

--- Fundamentals with Python - Sept 2023/15 Function - Exercise/10_perfect_number.py
+++ Fundamentals with Python - Sept 2023/15 Function - Exercise/10_perfect_number.py
@@ -1,12 +1,12 @@
-def perf_number(some_number):
-    sum = 0
-    for divisor in range(1,some_number):
-        if some_number % divisor == 0:
-            sum += divisor
-    if sum == some_number:
-        print("We have a perfect number!")
+def perfect_number(num:int):
+    sum_counter = 0
+    for cur_num in range(1, num):
+        if num % cur_num == 0 and cur_num > 0:
+            sum_counter += cur_num
+    if num == sum_counter:
+        return "We have a perfect number!"
     else:
-        print("It's not so perfect.")
+        return "It's not so perfect."
 
 number = int(input())
-perf_number(number)
+print(perfect_number(number))
