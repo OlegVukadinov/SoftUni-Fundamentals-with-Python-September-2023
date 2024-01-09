f0f2cf10fbd7cf1b25c8262f5ecd000ab70a2362
Oleg Vukadinov <153370690+OlegVukadinov@users.noreply.github.com>
2024-01-09 10:15:57 +0200

    edited solutions

--- Fundamentals with Python - Sept 2023/26 Dictionaries - Exercise/3_capitals.py
+++
@@ -1,7 +0,0 @@
-country_list = input().split(", ")
-capital_list = input().split(", ")
-
-my_dict = dict(zip(country_list, capital_list))
-
-for country, capital in my_dict.items():
-    print(f"{country} -> {capital}")
