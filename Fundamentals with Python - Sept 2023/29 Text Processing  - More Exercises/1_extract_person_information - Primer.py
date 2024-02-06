4b3efd3c3ab5d503bd49ac52c6e454aee5f282fa
Oleg Vukadinov <153370690+OlegVukadinov@users.noreply.github.com>
2024-02-06 10:21:25 +0200

    changed solutions

--- Fundamentals with Python - Sept 2023/29 Text Processing  - More Exercises/1_extract_person_information - Primer.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-
-for curr_n in range(n):
-    text = input()
-    name = text.split("@")[1].split("|")[0]
-    age = text.split("#")[1].split("*")[0]
-
-    print(f"{name} is {age} years old.")
